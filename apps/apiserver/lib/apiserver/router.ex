f7ed447e4f14f78e9c43bc7ece7191ab567b7d9e
Farsheed Ashouri <rodmena@me.com>
2018-10-22 02:07:16 +0330

    fixes #6

--- apps/apiserver/lib/apiserver/router.ex
+++ apps/apiserver/lib/apiserver/router.ex
@@ -1,11 +1,13 @@
 defmodule Apiserver.Router do
   use Apiserver, :router
+  @api_version 2
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
-  scope "/api", Apiserver do
-    pipe_through :api
+  scope "/api/v#{@api_version}/monitoring", Apiserver do
+    pipe_through(:api)
+    get("/ping", MonitoringController, :ping)
   end
 end
