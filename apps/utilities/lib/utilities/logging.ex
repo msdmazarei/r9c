a0c863a70775fd95eca79bad46f09ded68e19d1a
Masoud Mazarei <msd.mazarei@gmail.com>
2018-10-27 20:45:46 +0330

    [Utilities.Logging] - add simple debug macro to wrap logger and make logging easy to humans

---
+++ apps/utilities/lib/utilities/logging.ex
@@ -0,0 +1,34 @@
+defmodule Utilities.Logging do
+  @moduledoc false
+  require Logger
+
+  defmacro debug(message) do
+
+    quote do
+      Logger.debug(
+        fn ->
+          format_str = "<~p>(~p)[~p]" <> unquote(message)
+          :io_lib.format(format_str, [self(), __MODULE__, __ENV__.function])
+        end
+      )
+    end
+
+
+  end
+
+  defmacro debug(message, gholi) do
+
+
+    quote do
+      Logger.debug(
+        fn ->
+          format_str = "<~p>(~p)[~p]-  " <> unquote(message)
+          l = [self(), __MODULE__, __ENV__.function ] ++ unquote(gholi)
+          :io_lib.format(format_str, l)
+        end
+      )
+    end
+
+  end
+
+end
