02de027c230ce360e716117f9d47e95504199f43
Masoud Mazarei <msd.mazarei@gmail.com>
2018-10-28 12:44:44 +0330

    [DatabaseEngine.DurableQueue.Consumers.SimpleLogConsumer] - add state to Log

--- apps/database_engine/lib/database_engine/simple_log_consumer.ex
+++ apps/database_engine/lib/database_engine/simple_log_consumer.ex
@@ -13,16 +13,16 @@ defmodule DatabaseEngine.DurableQueue.Consumers.SimpleLogConsumer do
 
   # note - messages are delivered in batches
   def handle_message_set(message_set, state) do
-    KafkaEx.metadata()
     for %Message{value: message} <- message_set do
-
       Logging.debug(
-        "Message:~p Deserialized:~p",
+        "Message:~p Deserialized:~p state:~p",
         [
           message,
-          DatabaseEngine.DurableQueue.deserialize(message)
+          DatabaseEngine.DurableQueue.deserialize(message),
+        state
         ]
       )
+#      Process.sleep(10000)
 
     end
     {:async_commit, state}
