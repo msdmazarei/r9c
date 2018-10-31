553d34ebb164983613b0552d5e9c5c2620ca4a63
Masoud Mazarei <msd.mazarei@gmail.com>
2018-10-31 16:19:52 +0330

    [GatewayCore.GSMModemGateway] - #8 - add Gateway module

--- apps/gateway_core/lib/gateway_core/gsm_modem.ex
+++ apps/gateway_core/lib/gateway_core/gsm_modem.ex
@@ -13,3 +13,14 @@ defmodule GatewayCore.GSMModemGateway.Supervisor do
   end
 
 end
+
+
+defmodule GatewayCore.GSMModemGateway do
+  def send_sms(target_no, body) do
+    gsm_gateway = Application.get_env(:gateway_core, :gsm_gateway)
+    DatabaseEngine.DurableQueue.enqueue(
+      gsm_gateway[:q_out],
+      %DatabaseEngine.Models.SMS{receiver: target_no, body: body}
+    )
+  end
+end
