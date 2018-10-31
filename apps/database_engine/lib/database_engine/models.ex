b6a251253a1a905c0e9d6a44922a3a12035f7f33
Masoud Mazarei <msd.mazarei@gmail.com>
2018-10-31 15:35:50 +0330

    [DatabaseEngine.Models.SMS] - #8 - a SMS model added to project

---
+++ apps/database_engine/lib/database_engine/models.ex
@@ -0,0 +1,18 @@
+defmodule DatabaseEngine.Models do
+  @moduledoc false
+
+
+  defmodule SMS do
+    @derive Jason.Encoder
+    defstruct sender: "" ,
+              receiver: "",
+              sms_center: "",
+              sent_epoch: 0,
+              received_epoch: 0,
+              body: ""
+
+  end
+
+
+
+end
