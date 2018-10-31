42854eca296c0990f47cc064d4e3eeb2a538bbfe
Masoud Mazarei <msd.mazarei@gmail.com>
2018-10-31 15:39:30 +0330

    [GatewayCore.GSMModemGateway.Supervisor] - #8 - supervisor for gsm modems (erl_gsm workers) is added to project

---
+++ apps/gateway_core/lib/gateway_core/gsm_modem.ex
@@ -0,0 +1,15 @@
+defmodule GatewayCore.GSMModemGateway.Supervisor do
+  @moduledoc false
+
+  use DynamicSupervisor
+
+  def start_link(arg) do
+    DynamicSupervisor.start_link(__MODULE__, arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_arg) do
+    DynamicSupervisor.init(strategy: :one_for_one)
+  end
+
+end
