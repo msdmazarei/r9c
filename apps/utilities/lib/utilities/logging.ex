5fcc12bd4d6402ff8c1a836ccaceb10cc36164e7
Masoud Mazarei <msd.mazarei@gmail.com>
2018-11-11 19:22:40 +0330

    [Utilities.Logging] - #16 - info,warn,error levels are added

--- apps/utilities/lib/utilities/logging.ex
+++ apps/utilities/lib/utilities/logging.ex
@@ -20,4 +20,61 @@ defmodule Utilities.Logging do
       end)
     end
   end
+
+  defmacro info(message) do
+    quote do
+      Logger.info(fn ->
+        format_str = "<~p>(~p)[~p]" <> unquote(message)
+        :io_lib.format(format_str, [self(), __MODULE__, __ENV__.function])
+      end)
+    end
+  end
+
+  defmacro info(message, gholi) do
+    quote do
+      Logger.info(fn ->
+        format_str = "<~p>(~p)[~p]-  " <> unquote(message)
+        l = [self(), __MODULE__, __ENV__.function] ++ unquote(gholi)
+        :io_lib.format(format_str, l)
+      end)
+    end
+  end
+
+  defmacro warn(message) do
+    quote do
+      Logger.warn(fn ->
+        format_str = "<~p>(~p)[~p]" <> unquote(message)
+        :io_lib.format(format_str, [self(), __MODULE__, __ENV__.function])
+      end)
+    end
+  end
+
+  defmacro warn(message, gholi) do
+    quote do
+      Logger.warn(fn ->
+        format_str = "<~p>(~p)[~p]-  " <> unquote(message)
+        l = [self(), __MODULE__, __ENV__.function] ++ unquote(gholi)
+        :io_lib.format(format_str, l)
+      end)
+    end
+  end
+
+  defmacro error(message) do
+    quote do
+      Logger.error(fn ->
+        format_str = "<~p>(~p)[~p]" <> unquote(message)
+        :io_lib.format(format_str, [self(), __MODULE__, __ENV__.function])
+      end)
+    end
+  end
+
+  defmacro error(message, gholi) do
+    quote do
+      Logger.error(fn ->
+        format_str = "<~p>(~p)[~p]-  " <> unquote(message)
+        l = [self(), __MODULE__, __ENV__.function] ++ unquote(gholi)
+        :io_lib.format(format_str, l)
+      end)
+    end
+  end
 end
