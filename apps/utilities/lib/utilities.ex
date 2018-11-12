9230acb2340270f1a68ee75f57ee2de5b12399a6
Masoud Mazarei <msd.mazarei@gmail.com>
2018-11-12 13:44:38 +0330

    [Utilities] - #16 - to_struct function is added

--- apps/utilities/lib/utilities.ex
+++ apps/utilities/lib/utilities.ex
@@ -73,4 +73,15 @@ defmodule Utilities do
   end
 
   def nested_tuple_to_list(x), do: x
+
+  def to_struct(kind, attrs) do
+    struct = struct(kind)
+
+    Enum.reduce(Map.to_list(struct), struct, fn {k, _}, acc ->
+      case Map.fetch(attrs, Atom.to_string(k)) do
+        {:ok, v} -> %{acc | k => v}
+        :error -> acc
+      end
+    end)
+  end
 end
