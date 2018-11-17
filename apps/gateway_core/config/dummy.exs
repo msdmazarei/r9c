c9f55a947350ec85f8425c18adb5f25036627402
Masoud Mazarei <msd.mazarei@gmail.com>
2018-11-17 20:53:14 +0330

    add dummy gateway

---
+++ apps/gateway_core/config/dummy.exs
@@ -0,0 +1,9 @@
+use Mix.Config
+
+config :gateway_core, Red9Cobra.DUMMY,
+  name: "DUMMY",
+  input_Q: System.get_env("DUMMY_IN_Q") || "dummy_input_q",
+  success_Q: System.get_env("DUMMY_SUCCESS_Q") || "dummy_success_q",
+  fail_Q: System.get_env("DUMMY_FAIL_Q") || "dummy_fail_q",
+  crash_probeblity: System.get_env("DUMMY_CRASH_PROBEBLITY") || 0.1,
+  send_failur_probeblity: System.get_env("DUMMY_SEND_PROBEBLITY") || 0.1
