94cacf5da05a2178d883ead81af2998abb9e9b58
Masoud Mazarei <msd.mazarei@gmail.com>
2018-12-03 18:10:39 +0330

    [Dispatcher] - #23 - add dispatcher consumer

--- apps/dispatcher/config/config.exs
+++ apps/dispatcher/config/config.exs
@@ -28,3 +28,9 @@ use Mix.Config
 # here (which is why it is important to import them last).
 #
 #     import_config "#{Mix.env()}.exs"
+
+
+config :dispatcher,
+       input_queues: [
+         "in_dummy","in_imi","in_irmtn"
+       ]
