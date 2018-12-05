a32afed66ed8232d453e83b7c9438db83497bd0b
Masoud Mazarei <msd.mazarei@gmail.com>
2018-12-05 19:19:24 +0330

    [GatewayCore] - #16 - start gateways on app start up

--- apps/gateway_core/lib/gateway_core/application.ex
+++ apps/gateway_core/lib/gateway_core/application.ex
@@ -21,54 +21,89 @@ defmodule GatewayCore.Application do
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: GatewayCore.Supervisor]
     rtn = Supervisor.start_link(children, opts)
+    # will supervisor by genral consumer supervisors
+    GatewayCore.Outputs.Dummy.start()
+    GatewayCore.Outputs.IMI.start()
+    GatewayCore.Outputs.IrMtn.start()
+
     run_gsm_modems_gateway()
     rtn
   end
 
   def run_gsm_modems_gateway() do
-    Logging.debug("config for modems are:~p", [Application.get_env(:gateway_core, :modems)])
+    Logging.debug("Called node:~p", [to_string(node())])
+    c = Application.get_env(:gateway_core, GatewayCore.Drivers.GsmModemDriver.Output)
+    node_config = c[node()]
+    Logging.debug("Node Config: ~p", [node_config])
 
-    for modem <- Application.get_env(:gateway_core, :modems) do
-      add_gsm_modem(modem)
-    end
-
-    gsm_gateway = Application.get_env(:gateway_core, :gsm_gateway)
-    Logging.debug("Config for gsm_gateway:~p", [gsm_gateway])
-
-    if gsm_gateway != nil do
-      Logging.debug("gsm_gateway is not nil so check output queue, output queue: ~p", [
-        gsm_gateway[:q_out]
-      ])
-
-      if gsm_gateway[:q_out] != nil do
-        Logging.debug("gsm_gateway has output queue so start its consumer...")
-
-        DatabaseEngine.DurableQueue.start_consumer_group(
-          gsm_gateway[:q_out],
-          gsm_gateway[:q_out] <> "_consumer",
-          GatewayCore.Drivers.GsmModemDriver.Output
-        )
+    if node_config == nil do
+      true
+    else
+      for modem <- node_config[:modems] do
+        add_gsm_modem(modem)
       end
 
-      if gsm_gateway[:q_in] != nil do
-        Logging.debug(
-          "gsm_gateway has input queue so register to all incomping messages from modems..."
-        )
+      GatewayCore.Drivers.GsmModemDriver.Output.start()
 
+      if node_config[:ingress_Q] != nil do
         for {_, p, _, _} <-
               DynamicSupervisor.which_children(GatewayCore.GSMModemGateway.Supervisor) do
           :simple_gsm_modem_over_tcp.register_to_inform_every_message(
             p,
             GatewayCore.Drivers.GsmModemDriver.Input,
             :receive_sms,
-            [gsm_gateway[:q_in]]
+            [node_config[:ingress_Q]]
           )
         end
       end
     end
+
+    #
+    #    Logging.debug("config for modems are:~p", [Application.get_env(:gateway_core, :modems)])
+    #
+    #    for modem <- Application.get_env(:gateway_core, :modems) do
+    #      add_gsm_modem(modem)
+    #    end
+    #
+    #    gsm_gateway = Application.get_env(:gateway_core, :gsm_gateway)
+    #    Logging.debug("Config for gsm_gateway:~p", [gsm_gateway])
+    #
+    #    if gsm_gateway != nil do
+    #      Logging.debug("gsm_gateway is not nil so check output queue, output queue: ~p", [
+    #        gsm_gateway[:q_out]
+    #      ])
+    #
+    #      if gsm_gateway[:q_out] != nil do
+    #        Logging.debug("gsm_gateway has output queue so start its consumer...")
+    #
+    #        DatabaseEngine.DurableQueue.start_consumer_group(
+    #          gsm_gateway[:q_out],
+    #          gsm_gateway[:q_out] <> "_consumer",
+    #          GatewayCore.Drivers.GsmModemDriver.Output
+    #        )
+    #      end
+    #
+    #      if gsm_gateway[:q_in] != nil do
+    #        Logging.debug(
+    #          "gsm_gateway has input queue so register to all incomping messages from modems..."
+    #        )
+    #
+    #        for {_, p, _, _} <-
+    #              DynamicSupervisor.which_children(GatewayCore.GSMModemGateway.Supervisor) do
+    #          :simple_gsm_modem_over_tcp.register_to_inform_every_message(
+    #            p,
+    #            GatewayCore.Drivers.GsmModemDriver.Input,
+    #            :receive_sms,
+    #            [gsm_gateway[:q_in]]
+    #          )
+    #        end
+    #      end
+    #    end
   end
 
   def add_gsm_modem({address, port}) do
+    Logging.debug("Called address:~p,port:~p", [address, port])
+
     child_spec = %{
       :id => "modem-#{address}-#{port}",
       :start => {:simple_gsm_modem_over_tcp, :start_link, [:binary.bin_to_list(address), port]},
