f7ceab84e2d708450cabeab94bf3c6fdebc79b03
Masoud Mazarei <msd.mazarei@gmail.com>
2018-12-19 18:12:19 +0330

    [GatewayCore.Utils] - throttle added

--- apps/gateway_core/lib/utils/throttle.ex
+++ apps/gateway_core/lib/utils/throttle.ex
@@ -6,7 +6,45 @@ defmodule GatewayCore.Utils.Throttle do
   alias Utilities.Logging
   require DatabaseEngine.Interface.KV
   alias DatabaseEngine.Interface.KV
+  alias :mnesia, as: Mnesia
+
   @throttle_slice_count 5
+
+  defp get_key(n, c, t) do
+    "throttle-#{n}-#{c}-#{t}"
+  end
+
+  def multiple_is_allowed(throttle_name, list_how_many_how_much) do
+    if KV.get("multiple-#{throttle_name}") == nil do
+      list_how_many_how_much
+      |> Enum.map(fn {co, ti} ->
+        KV.set(get_key(throttle_name, co, ti), init_throttle_state(co, ti))
+      end)
+
+      KV.set("multiple-#{throttle_name}", true)
+    end
+
+    res =
+      case Mnesia.transaction(fn ->
+             rtn =
+               list_how_many_how_much
+               |> Enum.reduce(true, fn {count, time}, r ->
+                 r and check_is_allowed(throttle_name, count, time)
+               end)
+
+             if rtn == false do
+               :mnesia.abort(rtn)
+             else
+               rtn
+             end
+           end) do
+        {:atomic, true} -> true
+        _ -> false
+      end
+
+    Logging.debug("returns:~p", [res])
+    res
+  end
 
   @spec check_is_allowed(String.t(), integer(), integer()) :: boolean()
   def check_is_allowed(throttle_name, how_many_allowed, how_much_time) do
@@ -15,11 +53,13 @@ defmodule GatewayCore.Utils.Throttle do
 
     throttle_state =
       case KV.get(key) do
-        {_, nil} ->
+        nil ->
           s = init_throttle_state(how_many_allowed, how_much_time)
-          KV.set(key,s)
+          KV.set(key, s)
           s
-        {_, v} -> v
+
+        v ->
+          v
       end
 
     unix_time_now = Utilities.now()
@@ -27,8 +67,8 @@ defmodule GatewayCore.Utils.Throttle do
     Logging.debug("throttle_state:~p", [throttle_state])
 
     time_offset = unix_time_now - throttle_state.start_time
-    now_index = div(time_offset * @throttle_slice_count, how_much_time )
-    available_quota = now_index  * throttle_state.each_division_count
+    now_index = div(time_offset * @throttle_slice_count, how_much_time)
+    available_quota = now_index * throttle_state.each_division_count
 
     Logging.debug("time offset:~p now_index:~p", [time_offset, now_index])
 
@@ -59,10 +99,10 @@ defmodule GatewayCore.Utils.Throttle do
         false
 
       _ ->
-        {wc,tt_now, correct_parts} =
+        {wc, tt_now, correct_parts} =
           case correct_parts |> length() do
             l when l >= now_index ->
-              {throttle_state.window_count+1,throttle_state.start_time,
+              {throttle_state.window_count + 1, throttle_state.start_time,
                correct_parts |> List.update_at(now_index, fn x -> x + 1 end)}
 
             _ ->
@@ -71,13 +111,24 @@ defmodule GatewayCore.Utils.Throttle do
                   Enum.at(correct_parts, i) || 0
                 end
 
-              {0,unix_time_now, c}
+              {0, unix_time_now, c}
           end
 
-        throttle_state = Map.merge(throttle_state, %{parts: correct_parts, start_time: tt_now,total_done: throttle_state.total_done+1,window_count: wc})
-#        if rem(throttle_state.total_done,101) == 0 do
-          Logging.info("thorttle_state.total_done:~p, window_c:~p", [throttle_state.total_done+1,throttle_state.window_count])
-#        end
+        throttle_state =
+          Map.merge(throttle_state, %{
+            parts: correct_parts,
+            start_time: tt_now,
+            total_done: throttle_state.total_done + 1,
+            window_count: wc
+          })
+
+        #        if rem(throttle_state.total_done,101) == 0 do
+        Logging.debug("thorttle_state.total_done:~p, window_c:~p", [
+          throttle_state.total_done + 1,
+          throttle_state.window_count
+        ])
+
+        #        end
         KV.set(key, throttle_state)
         true
     end
@@ -89,18 +140,48 @@ defmodule GatewayCore.Utils.Throttle do
       count: @throttle_slice_count,
       parts: [],
       start_time: Utilities.now(),
-    total_done: 0,
-    window_count: 0
+      total_done: 0,
+      window_count: 0
     }
   end
 
   def f(name) do
-    check_is_allowed(name,6000,60_000)
+    check_is_allowed(name, 6000, 60_000)
   end
+
   def g(name) do
     for _ <- 0..10000 do
       f(name)
       Process.sleep(1)
+    end
+  end
+
+  def counter_process(v) do
+    receive do
+      :end -> Logging.debug("counter receiver messages:~p", [v])
+      1 ->
+        Logging.info("t:~p,inc",[Utilities.now()])
+        counter_process(v + 1)
+      _ -> counter_process(v)
+    end
+  end
+
+  def test_1(runtime) do
+    pid = Kernel.spawn(fn -> counter_process(0) end)
+    test_1(Utilities.now() + runtime, pid)
+  end
+
+  def test_1(ent, pid) do
+    if Utilities.now() > ent do
+      send(pid, :end)
+    else
+      if multiple_is_allowed("gholi-k", [{3, 10_000},{9,60_000}]) do
+        send(pid, 1)
+      else
+        Process.sleep(1000)
+      end
+
+      test_1(ent, pid)
     end
   end
 end
